b34fa37bc93b1cfa5aca968d0251971682968460
Vincent Berthiaume <vberthiaume@gmail.com>
2015-04-20 16:33:45 -0400

    moved voices and sounds to their own file

---
+++ Source/sBMP4SoundsAndVoices.cpp
@@ -0,0 +1,177 @@
+/*
+==============================================================================
+sBMP4: kilker subtractive synth!
+
+Copyright (C) 2014  BMP4
+
+Developer: Vincent Berthiaume
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+==============================================================================
+*/
+
+#include "sBMP4SoundsAndVoices.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+Bmp4SynthVoice::Bmp4SynthVoice()
+	: m_dOmega(0.0),
+	m_dTailOff(0.0)
+{
+}
+
+void Bmp4SynthVoice::renderNextBlock(AudioSampleBuffer& p_oOutputBuffer, int p_iStartSample, int p_iTotalSamples)  {
+	if (m_dOmega == 0.0) {
+		return;
+	}
+
+	double dTailOffCopy;
+
+	for (int iCurSample = 0; iCurSample < p_iTotalSamples; ++iCurSample) {
+
+		//this will be == 1 if we don't have a tail off or = m_dTailOff if we do
+		dTailOffCopy = m_dTailOff > 0 ? m_dTailOff : 1;
+
+		const float fCurrentSample = getSample(m_dCurrentAngle, m_dLevel, dTailOffCopy);
+
+		for (int i = p_oOutputBuffer.getNumChannels(); --i >= 0;){
+			p_oOutputBuffer.addSample(i, p_iStartSample, fCurrentSample);
+		}
+
+		m_dCurrentAngle += m_dOmega;
+		++p_iStartSample;
+
+		if (m_dTailOff > 0) {
+			m_dTailOff *= 0.99;
+
+			if (m_dTailOff <= 0.005) {
+				clearCurrentNote();
+
+				m_dOmega = 0.0;
+				break;
+			}
+		}
+	}
+}
+
+void Bmp4SynthVoice::startNote(int midiNoteNumber, float velocity, SynthesiserSound* sound, int /*currentPitchWheelPosition*/)  {
+	m_dCurrentAngle = 0.0;
+	m_dLevel = velocity * 0.15;
+	m_dTailOff = 0.0;
+
+	double dFrequency = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+	double dNormalizedFreq = dFrequency / getSampleRate();
+	m_dOmega = dNormalizedFreq * 2.0 * double_Pi;
+
+	m_oCurrentSynthSound = dynamic_cast<SynthesiserSound*>(sound);
+}
+
+void Bmp4SynthVoice::stopNote(float /*velocity*/, bool allowTailOff)  {
+
+	if (allowTailOff) {
+		// start a tail-off by setting this flag. The render callback will pick up on
+		// this and do a fade out, calling clearCurrentNote() when it's finished.
+
+		if (m_dTailOff == 0.0)	// we only need to begin a tail-off if it's not already doing so - the
+			// stopNote method could be called more than once.
+			m_dTailOff = 1.0;
+	} else {
+		// we're being told to stop playing immediately, so reset everything..
+
+		clearCurrentNote();
+		m_dOmega = 0.0;
+	}
+}
+
+bool SineWaveVoice::canPlaySound(SynthesiserSound* sound)  {
+
+	if (dynamic_cast <SineWaveSound*> (sound)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+float SineWaveVoice::getSample(double p_dAngle, double p_dLevel, double dTail) {
+
+	return (float)(sin(p_dAngle) * p_dLevel * dTail);
+}
+
+JUCE_COMPILER_WARNING("Would probably be way more efficient to use wave tables for all these additive synthesis getSamples in square, triangle and sawtooth")
+
+	SquareWaveVoice::SquareWaveVoice()
+	:m_iK(50)
+{
+}
+
+bool SquareWaveVoice::canPlaySound(SynthesiserSound* sound) {
+
+	if (dynamic_cast <SquareWaveSound*> (sound)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+float SquareWaveVoice::getSample(double p_dAngle, double p_dLevel, double dTail) {
+	float fCurrentSample = 0.0;
+	for (int iCurK = 0; iCurK < m_iK; ++iCurK){
+		fCurrentSample += static_cast<float> (sin(m_dCurrentAngle * (2 * iCurK + 1)) / (2 * iCurK + 1));
+	}
+	return fCurrentSample * p_dLevel * dTail;
+}
+
+
+bool TriangleWaveVoice::canPlaySound(SynthesiserSound* sound) {
+
+	if (dynamic_cast <TriangleWaveSound*> (sound)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+float TriangleWaveVoice::getSample(double p_dAngle, double p_dLevel, double dTail) {
+
+	float fCurrentSample = 0.0;
+
+	for (int iCurK = 0; iCurK < m_iK; ++iCurK){
+		fCurrentSample += static_cast<float> (sin(M_PI*(2 * iCurK + 1) / 2) * (sin(m_dCurrentAngle * (2 * iCurK + 1)) / pow((2 * iCurK + 1), 2)));
+	}
+
+	JUCE_COMPILER_WARNING(new std::string("p_dLevel doesn<t seem to work for any wave"))
+		return (8 / pow(M_PI, 2)) * fCurrentSample * p_dLevel * dTail;
+}
+
+
+bool SawtoothWaveVoice::canPlaySound(SynthesiserSound* sound)  {
+
+	if (dynamic_cast <SawtoothWaveSound*> (sound)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+float SawtoothWaveVoice::getSample(double p_dAngle, double p_dLevel, double dTail) {
+
+	float fCurrentSample = 0.0;
+
+	for (int iCurK = 1; iCurK < m_iK; ++iCurK){
+		fCurrentSample += static_cast<float> (sin((M_PI * iCurK) / 2) * (sin(m_dCurrentAngle * iCurK) / iCurK));
+	}
+
+	return (2 / M_PI) * fCurrentSample * p_dLevel * dTail;
+}
+
---
+++ Source/sBMP4SoundsAndVoices.h
@@ -0,0 +1,134 @@
+/*
+==============================================================================
+sBMP4: kilker subtractive synth!
+
+Copyright (C) 2014  BMP4
+
+Developer: Vincent Berthiaume
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+==============================================================================
+*/
+
+#ifndef sBMP4_Sounds_h
+#define sBMP4_Sounds_h
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+//Synth sounds
+
+class SineWaveSound : public SynthesiserSound
+{
+public:
+	SineWaveSound() {}
+
+	bool appliesToNote(int /*midiNoteNumber*/) override  { return true; }
+	bool appliesToChannel(int /*midiChannel*/) override  { return true; }
+};
+
+class SquareWaveSound : public SynthesiserSound
+{
+public:
+	SquareWaveSound() {}
+
+	bool appliesToNote(int /*midiNoteNumber*/) override  { return true; }
+	bool appliesToChannel(int /*midiChannel*/) override  { return true; }
+};
+
+class TriangleWaveSound : public SynthesiserSound
+{
+public:
+	TriangleWaveSound() {}
+
+	bool appliesToNote(int /*midiNoteNumber*/) override  { return true; }
+	bool appliesToChannel(int /*midiChannel*/) override  { return true; }
+};
+
+class SawtoothWaveSound : public SynthesiserSound
+{
+public:
+	SawtoothWaveSound() {}
+
+	bool appliesToNote(int /*midiNoteNumber*/) override  { return true; }
+	bool appliesToChannel(int /*midiChannel*/) override  { return true; }
+};
+
+
+class Bmp4SynthVoice : public SynthesiserVoice
+{
+public:
+	Bmp4SynthVoice();
+
+	// this is where we determine which unique sound this voice can play
+	bool canPlaySound(SynthesiserSound* sound) = 0;
+
+	virtual float getSample(double p_dAngle, double, double dTail) = 0;
+
+	void renderNextBlock(AudioSampleBuffer& p_oOutputBuffer, int p_iStartSample, int p_iTotalSamples) override;
+
+	void startNote(int midiNoteNumber, float velocity, SynthesiserSound* sound, int /*currentPitchWheelPosition*/) override;
+
+	void stopNote(float /*velocity*/, bool allowTailOff) override;
+
+	void pitchWheelMoved(int /*newValue*/) override {
+		// can't be bothered implementing this for the demo!
+	}
+
+	void controllerMoved(int /*controllerNumber*/, int /*newValue*/) override {
+		// not interested in controllers in this case.
+	}
+
+protected:
+	double m_dCurrentAngle, m_dOmega, m_dLevel, m_dTailOff;
+	SynthesiserSound* m_oCurrentSynthSound;
+};
+
+class SineWaveVoice : public Bmp4SynthVoice
+{
+	virtual bool canPlaySound(SynthesiserSound* sound) override;
+
+	virtual float getSample(double p_dAngle, double p_dLevel, double dTail) override;
+
+};
+
+class SquareWaveVoice : public Bmp4SynthVoice
+{
+public:
+	SquareWaveVoice();
+
+protected:
+	bool canPlaySound(SynthesiserSound* sound) override;
+
+	virtual float getSample(double p_dAngle, double p_dLevel, double dTail);
+
+	int m_iK;
+};
+
+class TriangleWaveVoice : public SquareWaveVoice
+{
+	virtual bool canPlaySound(SynthesiserSound* sound) override;
+
+	virtual float getSample(double p_dAngle, double p_dLevel, double dTail);
+
+};
+
+class SawtoothWaveVoice : public SquareWaveVoice
+{
+	virtual bool canPlaySound(SynthesiserSound* sound) override;
+
+	virtual float getSample(double p_dAngle, double p_dLevel, double dTail);
+};
+
+#endif //sBMP4_Sounds_h
