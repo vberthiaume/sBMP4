70f124597e259eea25cf278bd3c386460713a5c4
Vincent Berthiaume <vberthiaume@gmail.com>
2015-08-23 09:54:27 -0400

    some more debugging. i think problem is that average for first few values need to be done using values from last iteration, for each channel.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -229,7 +229,7 @@ void sBMP4AudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& m
 
 	//-----SIMPLESTLP
 	if(m_iFilterState != 0){
-		for(channel = 0; channel < getNumInputChannels(); ++channel) {
+		for(channel = 0; channel < 1/*getNumInputChannels()*/; ++channel) {
 			float* channelData = buffer.getWritePointer(channel);
 			simplestLP(channelData, numSamples);
 		}
@@ -259,25 +259,33 @@ void sBMP4AudioProcessor::simplestLP(float *p_fAllSamples, int p_iTotalSamples){
 		}
 	} else if(m_iFilterState == 2){
 		p_fAllSamples[1] = p_fAllSamples[0] / 2 + p_fAllSamples[1] / 2;
-		DBG(p_fAllSamples[1]);
+		if(p_fAllSamples[1] != 0)
+			DBG(p_fAllSamples[1]);
 		p_fAllSamples[2] = p_fAllSamples[0] / 3 + p_fAllSamples[1] / 3 + p_fAllSamples[2] / 3;
-		DBG(p_fAllSamples[2]);
+		if(p_fAllSamples[2] != 0)
+			DBG(p_fAllSamples[2]);
 		for(int iCurSpl = 3; iCurSpl < p_iTotalSamples-1; ++iCurSpl) {
 			p_fAllSamples[iCurSpl] = p_fAllSamples[iCurSpl-3]/4 + p_fAllSamples[iCurSpl-2]/4 + p_fAllSamples[iCurSpl-1]/4 + p_fAllSamples[iCurSpl]/4;
-			DBG(p_fAllSamples[iCurSpl]);
+			if(p_fAllSamples[iCurSpl] != 0)
+				DBG(p_fAllSamples[iCurSpl]);
 		}
 	} else if(m_iFilterState == 3){
 		p_fAllSamples[1] = p_fAllSamples[0] / 2 + p_fAllSamples[1] / 2;
-		DBG(p_fAllSamples[1]);
+		if(p_fAllSamples[1] != 0)
+			DBG(p_fAllSamples[1]);
 		p_fAllSamples[2] = p_fAllSamples[0] / 3 + p_fAllSamples[1] / 3 + p_fAllSamples[2] / 3;
-		DBG(p_fAllSamples[2]);
+		if(p_fAllSamples[2] != 0)
+			DBG(p_fAllSamples[2]);
 		p_fAllSamples[3] = p_fAllSamples[0]/4 + p_fAllSamples[1]/4 + p_fAllSamples[2]/4 + p_fAllSamples[3]/4;
-		DBG(p_fAllSamples[3]);
+		if(p_fAllSamples[3] != 0)
+			DBG(p_fAllSamples[3]);
 		p_fAllSamples[4] = p_fAllSamples[0] / 5 + p_fAllSamples[1] / 5 + p_fAllSamples[2] / 5 + p_fAllSamples[3] / 5 + p_fAllSamples[4] / 5;
-		DBG(p_fAllSamples[4]);
+		if(p_fAllSamples[4] != 0)
+			DBG(p_fAllSamples[4]);
 		for(int iCurSpl = 5; iCurSpl < p_iTotalSamples-1; ++iCurSpl) {
 			p_fAllSamples[iCurSpl] = p_fAllSamples[iCurSpl-5]/6 + p_fAllSamples[iCurSpl-4]/6 + p_fAllSamples[iCurSpl-3]/6 + p_fAllSamples[iCurSpl-2]/6 + p_fAllSamples[iCurSpl-1]/6 + p_fAllSamples[iCurSpl]/6;
-			DBG(p_fAllSamples[iCurSpl]);
+			if(p_fAllSamples[iCurSpl] != 0)
+				DBG(p_fAllSamples[iCurSpl]);
 		}
 	}
 }
