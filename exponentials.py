2a27f5d92f5f6224560fe3585ba98579d4507568
vberthiaume <vberthiaume@gmail.com>
2015-12-06 11:04:46 -0500

    exponential.py working

--- exponentials.py
+++ exponentials.py
@@ -1,16 +1,18 @@
-  
+import matplotlib.pyplot as plt
+import numpy as np
  
- import matplotlib.pyplot as plt
- import numpy as np
- 
- A = .8
- f0 = 1000
- phi = np.pi/2
- fs = 44100
- t = np.arange(-0.002, .002, 1.0/fs) #array of floats between -.002 and .002     +++in increments of 1/fs
- x = A * np.cos (2 * np.pi * f0 * t+ phi)
- plt.plot(t,x)
- plt.axis([-.002,.002,-.8,.8])
- plt.xlabel('time')
- plt.ylabel('amplitude')
- plt.show()
+t = np.arange(0, 1, .001)
+
+minFr = 400	#this should probably be the f0 of the pressed key, yes, that's exatly what it should be
+maxFr = 20000
+
+exponent = np.log(maxFr)
+
+exp = np.exp(exponent*t)
+
+
+plt.plot(t,exp)
+# plt.axis([-.1,.1.1,-.8,.8])
+# plt.xlabel('time')
+# plt.ylabel('amplitude')
+plt.show()
